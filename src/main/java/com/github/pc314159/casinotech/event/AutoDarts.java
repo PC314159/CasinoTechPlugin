104901745d853f6bd28c7af5d29a88ef48984c4c
BuildTools <unconfigured@null.spigotmc.org>
2024-04-08 01:10:48 -0700

    version 1.1 - pick up and bet any slot in hotbar

--- src/main/java/com/github/pc314159/casinotech/event/AutoDarts.java
+++ src/main/java/com/github/pc314159/casinotech/event/AutoDarts.java
@@ -12,58 +12,51 @@ import net.minecraft.text.Text;
 
 public class AutoDarts {
     public static int previous = -1;
+    public static int placebetcd = 0;
 
     public static void register(CasinoTechClient ctc) {
         ClientTickEvents.START_CLIENT_TICK.register(client -> {
             if (ctc.isModActive() && GuiUtils.getCurrentScreen() instanceof GenericContainerScreen) {
                 GenericContainerScreen screen = (GenericContainerScreen) GuiUtils.getCurrentScreen();
                 GenericContainerScreenHandler handler = screen.getScreenHandler();
-                Inventory inv = handler.getInventory();
 
                 if (screen.getTitle().getString().equals(Text.of("            投镖游戏").getString())) {
-//                    client.player.sendMessage(Text.of("in darts"));
+//                    currently in darts game
                     if (handler.slots.get(34).getStack().getName().getString().equals(Text.of("下注以开始旋转!").getString())) {
-//                        client.player.sendMessage(Text.of("awaiting"));
+//                        place bet in slot
                         if (handler.getCursorStack().getItem().equals(Items.DIAMOND)) {
-                            if (handler.slots.get(72).getStack().getItem().equals(Items.DIAMOND)) {
-                                System.out.println(handler.slots.get(72).getStack());
-//                                client.player.sendMessage(Text.of("toss"));
-                                client.interactionManager.clickSlot(handler.syncId, handler.EMPTY_SPACE_SLOT_INDEX, 0, SlotActionType.PICKUP, client.player);
-                            }
-                            else{
-                                System.out.println(handler.slots.get(72).getStack());
-//                                client.player.sendMessage(Text.of("place"));
-                                client.interactionManager.clickSlot(handler.syncId, 25, 0, SlotActionType.PICKUP, client.player);
-                            }
+                            client.interactionManager.clickSlot(handler.syncId, 25, 0, SlotActionType.PICKUP, client.player);
+                            placebetcd = 10;
                         }
-                        else{
-                            if (handler.slots.get(73).getStack().getItem().equals(Items.DIAMOND)) {
-                                System.out.println(handler.slots.get(73).getStack());
-//                                client.player.sendMessage(Text.of("pickup extra"));
-                                client.interactionManager.clickSlot(handler.syncId, 73, 0, SlotActionType.PICKUP, client.player);
-                            }
-                            else if (handler.slots.get(72).getStack().getItem().equals(Items.DIAMOND)) {
-                                System.out.println(handler.slots.get(72).getStack());
-//                                client.player.sendMessage(Text.of("pickup"));
-                                client.interactionManager.clickSlot(handler.syncId, 72, 0, SlotActionType.PICKUP, client.player);
+//                        get first slot of chips
+                        else if (placebetcd == 0) {
+                            for (int i = 72; i < 81; i++) {
+                                if (handler.slots.get(i).getStack().getItem().equals(Items.DIAMOND)) {
+                                    if (handler.slots.get(i).getStack().getName().getString().equals(Text.of("筹码").getString())) {
+                                        client.interactionManager.clickSlot(handler.syncId, i, 0, SlotActionType.PICKUP, client.player);
+                                        break;
+                                    }
+                                }
                             }
                         }
                     }
                     else if (handler.slots.get(34).getStack().getName().getString().equals(Text.of("点击旋转").getString())) {
                         client.interactionManager.clickSlot(handler.syncId, 34, 0, SlotActionType.PICKUP_ALL, client.player);
                     }
-                }
 
-
-                for (int i = 0; i < inv.size(); i++) {
-                    if (inv.getStack(i).getItem().equals(Items.PLAYER_HEAD)) {
-//                        client.player.sendMessage(Text.of("---------" + i));
-                        if (i != AutoDarts.previous) {
-                            AutoDarts.previous = i;
-//                            client.player.sendMessage(Text.of("clicked"));
-                            client.interactionManager.clickSlot(handler.syncId, handler.getSlot(i).id, 0, SlotActionType.PICKUP, client.player);
-                            break;
+                    Inventory inv = handler.getInventory();
+                    for (int i = 0; i < inv.size(); i++) {
+                        if (inv.getStack(i).getItem().equals(Items.PLAYER_HEAD)) {
+                            if (i != AutoDarts.previous) {
+                                AutoDarts.previous = i;
+                                client.interactionManager.clickSlot(handler.syncId, handler.getSlot(i).id, 0, SlotActionType.PICKUP_ALL, client.player);
+                                break;
+                            }
                         }
+                    }
+
+                    if (placebetcd > 0) {
+                        placebetcd -= 1;
                     }
                 }
             }
